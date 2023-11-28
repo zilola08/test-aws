cb103e4859268289a19e891da0414e93082b2f84
Zilola <44432264+zilola08@users.noreply.github.com>
2023-11-28 15:37:24 +0500

    feat: implement hello world app

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const PORT = 8080;
+const app = express();
+app.use(express.json());
+
+app.get('/',(req, res) => {
+  res.send('Hello AWS')
+});  
+
+const start = async () => {
+  try {
+    app.listen(PORT,() => {
+      console.log(`server started on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start();
+
+
+
+
+
+
+
