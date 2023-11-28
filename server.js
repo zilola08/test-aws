caecec990fe6d3a595f5157331d4694cbb6a1fde
Zilola <44432264+zilola08@users.noreply.github.com>
2023-11-28 20:46:01 +0500

    change port

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const PORT = 1337;
+const cors = require('cors');
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 app.get('/',(req, res) => {
   res.send('Hello AWS')
